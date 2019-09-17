38a0390dc334eebc2c9a01099da6450078c190f5
Anandraman <anandraman.7978@gmail.com>
2019-09-16 23:00:56 -0400

    Update upload_docker.sh

--- blue/upload_docker.sh
+++ blue/upload_docker.sh
@@ -6,13 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=testblueimage
+dockerpath=testbimage
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login --username anandraman7978 -p Panther@12
-docker tag testblueimage anandraman7978/testblueimage
+docker tag testbimage anandraman7978/testbimage
 # Step 3:
 # Push image to a docker repository
-docker push anandraman7978/testblueimage
+docker push anandraman7978/testbimage
