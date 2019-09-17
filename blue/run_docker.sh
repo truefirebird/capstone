9b57a9262f27604d24b80dd69ad41d5a43ce8256
Anandraman <anandraman.7978@gmail.com>
2019-09-16 22:28:05 -0400

    Update run_docker.sh

--- blue/run_docker.sh
+++ blue/run_docker.sh
@@ -12,4 +12,6 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-#docker run -p 8000:80 testblueimage
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker run -p 8000:80 testblueimage
